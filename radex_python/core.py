6e32b72e90f259446004fdcb4151cb7a9271667d
Magnus Vilhelm Persson <vilhelmp@gmail.com>
2013-11-14 15:16:34 +0100

    initian move of code

---
+++ radex_python/core.py
@@ -0,0 +1,162 @@
+
+
+
+
+
+class Radex:
+    """
+    Input (* = mandatory input)
+    *f      : Frequency - frequency of line (in GHz)
+    df      : Uncertainty of line position (defaults to 20 MHz)
+    *tkin   : Kinetic temperature  - one value or array (Kelvin)
+    *h2dens : H2 density one value or array (cm-3)
+    tbg     : Background temperature - defaults to 2.73 K
+    *oflux  : Observed flux in K km/s, to compare
+    *lwidth : Line width (FWHM)
+    *molf   : Molecular data file (LAMBDA format)
+    ftol    : How close can the flux be for it to be the same? (0.1)
+    maxiter : Maximum number of iterations (100)
+    eps     : Below what is zero or negat ive line intensity (1E-20)
+    cdens   : First guess molecular line density (1e12)
+    debug   : Output debug messages? (Boolean)
+    silent  : Output any msgs at all? (Boolean)
+    """
+    def __init__(self, **kwargs):
+        """
+        The program will take arrays as input.
+        So fx if you initialize it with
+        Radex(f = 100, tkin = arange(10), h2dens = 1e9, cdens=)
+
+        """
+        from scipy import array, arange
+        #~ from ..adacore import ParError as _ParError
+        #~ from multiprocessing import Pool
+        # check input parameters and store in class
+        # and raise ParError if wrong/missing
+        params = array(['silent', False, 'f', None, 'df', 0.02,
+                        'h2dens', None, 'tbg', 2.73, 'oflux', None,
+                        'lwidth', None, 'molf', None, 'tol', 0.1,
+                        'maxiter', 100, 'eps', 1.0e-20, 'cdinit', 1E12,
+                        'debug', False, 'tkin', None])
+        for par,stdval in zip(params[0::2],params[1::2]):
+            if par in kwargs:
+                if par == 'molf':
+                    self.molf = kwargs['molf']
+                elif par == 'tkin':
+                    if type(kwargs[par]) in [type([]), type(arange(1))]:
+                        self.tovary = 'tkin'
+                        self.tkin = kwargs[par]
+                    else:
+                        self.tkin = kwargs[par]
+                elif par == 'h2dens':
+                    if type(kwargs[par]) in [type([]), type(arange(1))]:
+                        if hasattr(self, 'tovary'):
+                            raise Exception('\'tkin\' and \'h2dens\' ',
+                                        'are both arrays, only one can',
+                                        ' be varied at a time')
+                        self.tovary = 'h2dens'
+                        self.h2dens = kwargs[par]
+                    else:
+                        self.h2dens = kwargs[par]
+                else:
+                    exec('self.{0} = {1}'.format(par, kwargs[par]))
+            else:
+                if stdval != None:
+                    exec('self.{0} = {1}'.format(par, stdval))
+                    if not self.silent:
+                        print('Using default value '
+                        'of {0} for {1}'.format(stdval, par))
+                else:
+                    raise Exception(par)
+    def __str__(self):
+        out_string = (40*'='+'\n'+
+                        'Frequency  : {0} GHz\n'.format(self.f)+
+                        'Bandwidth  : {0} MHz\n'.format(self.df*1E3)+
+                        'Kinetic T  : {0} K\n'.format(self.tkin)+
+                        'H2 density : {0} cm-3\n'.format(self.h2dens)+
+                        'Tbg        : {0} K\n'.format(self.tbg)+
+                        'Obs. Flux  : {0} K km s-1\n'.format(self.oflux)+
+                        'Line width : {0} km s-1\n'.format(self.lwidth)+
+                        'Moldata    : {0} \n'.format(self.molf)+
+                        'Tolerance  : {0} \n'.format(self.tol)+
+                        'Max. inter : {0} \n'.format(self.maxiter)+
+                        'Eps        : {0} \n'.format(self.eps)+
+                        'Init Cdens : {0} \n'.format(self.cdinit)+
+                        'Debug      : {0}'.format(self.debug))
+        return out_string
+    def _create_input(self):
+        with open('radex.inp','w') as f:
+            f.write('{0}\n'.format(self.molf))
+            f.write('radex.out\n')
+            f.write('{0} {1}\n'.format((self.f-self.df),
+                                        (self.f+self.df)))
+            f.write('{0}\n'.format(self.tkin))
+            f.write('1\n')
+            f.write('H2\n')
+            f.write('{0:3.2E}\n'.format(self.h2dens))
+            f.write('{0}\n'.format(self.tbg))
+            f.write('{0:3.2E}\n'.format(self.cdinit))
+            f.write('{0}\n'.format(self.lwidth))
+            f.write('0\n')
+    def _read_output(self):
+        import sys
+        with open('radex.out', 'r') as f:
+            lines = f.readlines()
+        if (lines[-2].split()[-1] != '(erg/cm2/s)'):
+            print "Error: Ambiguous line selection. Reduce bandwidth?"
+            print "See radex.out for details"
+            sys.exit()
+        return float(lines[-1].split()[-2])
+    def _find_cdens(self):
+        import os
+        ratio = 0
+        iteration = 0
+        while (ratio > (1+self.tol)) or (ratio < (1-self.tol)) :
+            iteration += 1
+            self._create_input()
+            os.system('radex < radex.inp > /dev/null')
+            mflux  = self._read_output()
+            if (mflux < self.eps):
+                msg = ("Zero or negative line intensity\n"
+                        "See radex.out for details")
+                if self.debug:
+                    raise Exception(msg)
+            if self.debug: print "mflx= ",mflux
+            ratio = self.oflux/[mflux,self.oflux][mflux==0]
+            self.cdinit *= ratio
+            if self.debug: print ratio
+            if (iteration > self.maxiter):
+                if not self.silent: print "Maximum number of iterations exceeded"
+                ratio = 1
+        self.mflux = mflux
+        return self.cdinit
+    def run_model(self):
+        from scipy import arange, array
+        #
+        #
+        if self.tovary == 'tkin':
+            tkin_inp = self.tkin
+            self.cdens = []
+            if not self.silent: print 'Running model and varying kinetic temperature.'
+            for i in tkin_inp:
+                self.tkin = i
+                density = self._find_cdens()
+                self.cdens.append(density)
+                if self.debug: print 'density : ',density
+            self.tkin = array(tkin_inp)
+        elif self.tovary == 'h2dens':
+            h2dens_inp = self.h2dens
+            self.cdens = []
+            if not self.silent: print 'Running model and varying H2 density.'
+            for i in h2dens_inp:
+                self.h2dens = i
+                density = self._find_cdens()
+                if self.debug: print 'density : ',density
+                self.cdens.append(density)
+            self.h2dens = array(h2dens_inp)
+        self.cdens = array(self.cdens)
+        if not self.silent: print 'Done!'
+
+
+
+
